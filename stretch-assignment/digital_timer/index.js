06ee57b1564e65bc5f2a205487cfd68900ab3931
UnknownMonk <smodcartoon@gmail.com>
2019-01-28 18:47:29 -0500

    added hundredes to the clock

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,15 +1,18 @@
 const tens = document.querySelector('#secondTens');
-let secondOnes = document.getElementById('secondOnes');
+let ones = document.querySelector('#secondOnes');
+let hundo = document.querySelector('#msHundreds');
 
 let counter = 0;
+let counterMs = 0;
+let counterHundo = 0;
 
 function timerSeconds() {
   if (counter < 9) {
     counter++;
     secondOnes.textContent = counter;
   } else {
-    secondOnes.innerHTML = '0';
-    secondOnes.style.color = 'red';
+    ones.innerHTML = '0';
+    ones.style.color = 'red';
   }
 }
 
@@ -17,5 +20,18 @@ function timerTen() {
   tens.textContent = 1;
   tens.style.color = 'red';
 }
+
 let tTen = setInterval(timerTen, 10000);
 let tSec = setInterval(timerSeconds, 1000);
+
+let thundo = setInterval(timerHuno, 10);
+
+function timerHuno() {
+  if (counterHundo < 9) {
+    counterHundo++;
+    hundo.textContent = counterHundo;
+  } else {
+    hundo.innerHTML = '0';
+    hundo.style.color = 'red';
+  }
+}
